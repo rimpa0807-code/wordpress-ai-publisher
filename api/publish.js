b24766d65e6ccfe7f7c026d964d4a58afea04be3
rimpa0807-code <rimpa0807@gmail.com>
2026-06-13 11:02:51 +0530

    Update publish.js

--- api/publish.js
+++ api/publish.js
@@ -423,6 +423,21 @@ Article Type: ${type}
     );
 
     const wpPost = await wpResponse.json();
+    await fetch("https://www.instapaper.com/api/add", {
+  method: "POST",
+  headers: {
+    Authorization:
+      "Basic " +
+      Buffer.from(
+        `${process.env.INSTAPAPER_USERNAME}:${process.env.INSTAPAPER_PASSWORD}`
+      ).toString("base64"),
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: new URLSearchParams({
+    url: wpPost.link,
+    title: article.title
+  })
+});
 
     // Mark Google Sheet Row as Published
     await fetch(process.env.SHEET_URL, {
